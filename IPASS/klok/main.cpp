32cd3e2bc1d1c2422afef572dd1d732b5aa71bad
Koen <koen@bravemail.nl>
2020-06-30 16:51:29 +0200

    Update main.cpp

--- IPASS/klok/main.cpp
+++ IPASS/klok/main.cpp
@@ -5,9 +5,10 @@
 
 int main(){
     namespace target = hwlib::target;
-
+    //to display the day
     const char *days[7] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saterday"};
     
+    //connection to the oled
     auto scl     = target::pin_oc( target::pins::scl );
     auto sda     = target::pin_oc( target::pins::sda );
     auto i2c_bus = hwlib::i2c_bus_bit_banged_scl_sda( scl, sda );
@@ -15,24 +16,26 @@ int main(){
 
     auto font    = hwlib::font_default_16x16(); //big fond for the numbers
     auto display = hwlib::terminal_from( oled, font );
-
+    
+    //connection to the ds1307 chip
     auto bus = r2d2::i2c::i2c_bus_c(r2d2::i2c::i2c_bus_c::interface::interface_0, 50000);
     auto clock = dsClock(bus, 40 ,59, 23, 3 , 30, 6, 20);
 
+    
     int minut_old = -1; //to prefent updating every time
     int hour_old = -1;
 
-    int minut_first = 0;
+    int minut_first = 0;    
     int minut_second = 0;
 
     int hour_first = 0;
     int hour_second = 0;
 
-    while(true){
+    while(true){    //infinty loop for updating time every 0.5 sec
         int minutes = clock.get_minutes();
  
         int hour = clock.get_hour();
-        hwlib::cout << clock.get_day();
+        //lib::cout << clock.get_day();
       
         //you do not want to update if it minutes are still the same
         if(minutes != minut_old){
