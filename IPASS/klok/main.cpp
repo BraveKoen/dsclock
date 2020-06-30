068ec2c675beed6eaf112863a19de390ea17b0ce
Koen <koen@bravemail.nl>
2020-06-30 19:29:24 +0200

    Update main.cpp

--- IPASS/klok/main.cpp
+++ IPASS/klok/main.cpp
@@ -1,3 +1,9 @@
+
+//          Copyright Koen Brave 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #include "ledscreen.hpp"
 #include "lednumber.hpp"
 #include "dsClock.hpp"
